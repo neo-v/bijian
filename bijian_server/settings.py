2ae87f078a6f1b544282353b7a53f1b4bc246517
root <root@iZ25vol6jrfZ.(none)>
2015-11-09 16:45:53 +0800

    connect to mysql

--- bijian_server/settings.py
+++ bijian_server/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'bijian_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	# 'ENGINE': 'django.db.backends.sqlite3',
+	# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME':'test_1234',
+	'USER':'test_1234', 
+   	'PASSWORD':'123456',
+	'HOST':'rdsv063y063k4cv6jkf3.mysql.rds.aliyuncs.com',
+	'PORT':'3306',	
+     }
 }
 
 
